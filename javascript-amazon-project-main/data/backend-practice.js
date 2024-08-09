c4ce68570440178f6e8608c098ba7bd5b703b75b
vedant <vedant2239@gmail.com>
2024-08-10 00:48:00 +0530

    learning backend

--- javascript-amazon-project-main/data/backend-practice.js
+++ javascript-amazon-project-main/data/backend-practice.js
@@ -1,3 +1,9 @@
 const xhr = new XMLHttpRequest();
-xhr.open('GET' , 'https://supersimplebackend.dev' );// two parameters first i sthe type of request and second to the place (url) to send the http message or request
+
+xhr.addEventListener('load' , ()=>{    // this is for waiting for the response from the backend as response works after some time so load means when it has loaded the response
+  console.log(xhr.response);
+});
+
+xhr.open('GET' , 'https://supersimplebackend.dev/documentation' );// two parameters first i sthe type of request and second to the place (url) to send the http message or request
 xhr.send();
+// xhr.response;
